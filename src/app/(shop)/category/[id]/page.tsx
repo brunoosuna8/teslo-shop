b7fa31a8ef58fc1d46ceefd0e17b8748d15e4722
brunoosuna8 <brunoosuna8@gmail.com>
2024-03-27 10:11:44 -0300

    add: sentry report bugs

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+import { captureException } from "@sentry/nextjs";
 import { notFound } from "next/navigation";
-
 interface Props {
   params: {
     id: string;
   };
 }
 export default function SingleCategoryPage({ params }: Props) {
+  const handleError = () => {
+    throw new Error("intentional error without capture");
+    // try {
+    //   throw new Error(`intentional error on ${id} category`);
+    // } catch (error) {
+    //   captureException(error);
+    // }
+  };
   const { id } = params;
   if (id === "kids") {
     notFound();
@@ -13,6 +22,7 @@ export default function SingleCategoryPage({ params }: Props) {
   return (
     <div>
       <h1>Single Category Page {id}</h1>
+      <button onClick={handleError}>Error Button on {id}</button>
     </div>
   );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,24 @@
+"use client";
 import ProductGrid from "@/components/products/product-grid/ProductGrid";
 import Title from "@/components/ui/title/Title";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import Image from "next/image";
-
+import * as Sentry from "@sentry/nextjs";
 const products = initialData.products;
+
 export default function Home() {
+  const handleError = () => {
+    try {
+      throw new Error("Intentional Error has been triggered");
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  };
   return (
     <div>
       <Title title={"Tienda"} subtitle="Todos los productos" className="mb-2" />
+      <button onClick={handleError}>Error Button</button>
       <ProductGrid products={products} />
     </div>
   );
